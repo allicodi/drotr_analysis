061a63cb2ee5631afb1bf7b0fd0838297f46a3d3
Allison Codi <allison.codi@emory.edu>
2026-05-27 21:51:46 -0400

    get data adaptive truth for sensitivity analyses

---
+++ sim_data/sub_jobs_data_adaptive_sens.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PARTITION=$1
+NSEEDS=$2
+SENS=$3
+
+sbatch --array=1-$NSEEDS \
+       --partition=$PARTITION \
+       -n 1 \
+       --output=/projects/dbenkes/allison/drotr_sim/journal/results_sim_data/scratch/%a_%J.out \
+       --job-name=run_data_adaptive_seed_%a \
+       --wrap "Rscript 12_get_data_adaptive_truth_sensitivity.R $SENS $SLURM_ARRAY_TASK_ID"
