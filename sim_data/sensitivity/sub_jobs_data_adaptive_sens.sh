c26eaaa3adca5be5ee2f2c1ab81c6800c5d7e124
allicodi <allicodi@gmail.com>
2026-05-29 12:50:15 -0400

    scratch name

--- sim_data/sensitivity/sub_jobs_data_adaptive_sens.sh
+++ sim_data/sensitivity/sub_jobs_data_adaptive_sens.sh
@@ -7,6 +7,6 @@ SENS=$3
 sbatch --array=1-$NSEEDS \
        --partition=$PARTITION \
        -n 1 \
-       --output=/projects/dbenkes/allison/drotr_sim/journal/results_sim_data/scratch/%a_%J.out \
+       --output=/projects/dbenkes/allison/drotr_sim/journal/results_sim_data/scratch/truth_%a_%J.out \
        --job-name=run_data_adaptive_seed_%a \
        --wrap "Rscript sensitivity/13_get_data_adaptive_truth_sensitivity.R $SENS $SLURM_ARRAY_TASK_ID"
