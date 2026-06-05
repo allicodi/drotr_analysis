23a261b0066156edd7a232a041310bd1217e30f9
Allison Codi <allison.codi@emory.edu>
2026-06-05 09:50:54 -0400

    timestamp

--- sim_data/sensitivity/12_make_results_table.R
+++ sim_data/sensitivity/12_make_results_table.R
@@ -19,7 +19,7 @@ for (miss_type in missing_types) {
   
   for (file in file_list) {
     
-    if (file.info(file)$mtime > as.POSIXct("2026-05-29")) {
+    if (file.info(file)$mtime > as.POSIXct("2026-06-04")) {
       
       data <- readRDS(file)
       
